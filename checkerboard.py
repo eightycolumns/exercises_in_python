823244d06a45c2597a6dee4853970524973281bb
Drew Keller <drew@eightycolumns.com>
2017-08-12 10:05:18 -0400

    Extract is_even()

--- checkerboard.py
+++ checkerboard.py
@@ -1,10 +1,16 @@
 import sys
 
-for i in range(0, 8):
-    for j in range(0, 8):
-        if (i + j) % 2 == 0:
-            sys.stdout.write('*')
-        else:
-            sys.stdout.write(' ')
+def main():
+    for i in range(0, 8):
+        for j in range(0, 8):
+            if is_even(i + j):
+                sys.stdout.write('*')
+            else:
+                sys.stdout.write(' ')
 
-    sys.stdout.write('\n')
+        sys.stdout.write('\n')
+
+def is_even(d):
+    return d % 2 == 0
+
+if __name__ == '__main__': main()
